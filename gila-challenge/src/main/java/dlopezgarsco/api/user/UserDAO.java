d5819b7b02b3c63e380c1666a4a35ca9cee4c113
Daniel Garsco <dlopezgarsco@gmail.com>
2023-08-13 14:15:20 -0300

    (server): UserDAO query added to retrieve all `Users` with both `Categories` and `Channels`.

--- gila-challenge/src/main/java/dlopezgarsco/api/user/UserDAO.java
+++ gila-challenge/src/main/java/dlopezgarsco/api/user/UserDAO.java
@@ -1,19 +1,46 @@
 package dlopezgarsco.api.user;
 
-import org.jdbi.v3.core.mapper.RowMapper;
-import org.jdbi.v3.core.statement.StatementContext;
-import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import dlopezgarsco.api.category.Category;
+import dlopezgarsco.api.channel.Channel;
+import org.jdbi.v3.core.result.LinkedHashMapRowReducer;
+import org.jdbi.v3.core.result.RowView;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.UseRowReducer;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
+@RegisterBeanMapper(value = User.class, prefix = "u")
+@RegisterBeanMapper(value = Category.class, prefix = "c")
+@RegisterBeanMapper(value = Channel.class, prefix = "ch")
 public interface UserDAO {
 
-    @SqlQuery("SELECT * FROM \"users\"")
-    @RegisterConstructorMapper(User.class)
-    List<User> selectAll();
+    @SqlQuery("SELECT u.user_id u_user_id, u.name u_name, u.email u_email, u.phone_number u_phone_number, " +
+            "c.category_id c_category_id, c.name c_name, " +
+            "ch.channel_id ch_channel_id, ch.name ch_name " +
+            "FROM users u " +
+            "NATURAL JOIN users_categories " +
+            "NATURAL JOIN users_channels " +
+            "JOIN categories c " +
+            "ON users_categories.category_id = c.category_id " +
+            "JOIN channels ch " +
+            "ON users_channels.channel_id = ch.channel_id")
+    @UseRowReducer(UserCategoryChannelReducer.class)
+    List<User> getUsersWithCategoriesAndChannels();
 
+    class UserCategoryChannelReducer implements LinkedHashMapRowReducer<Integer, User> {
+        @Override
+        public void accumulate(Map<Integer, User> map, RowView rv) {
+            User u = map.computeIfAbsent(rv.getColumn("u_user_id", Integer.class),
+                    id -> rv.getRow(User.class));
 
+            if (rv.getColumn("c_category_id", Integer.class) != null) {
+                u.getCategories().add(rv.getRow(Category.class));
+            }
+            if (rv.getColumn("ch_channel_id", Integer.class) != null) {
+                u.getChannels().add(rv.getRow(Channel.class));
+            }
+        }
+    }
 }
