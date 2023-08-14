6f66de57f20928187071f16b1aa847856d48a60f
Daniel Garsco <dlopezgarsco@gmail.com>
2023-08-13 21:44:00 -0300

    (server): ConstructorMapper -> BeanMapper

--- gila-challenge/src/main/java/dlopezgarsco/db/CategoryDAO.java
+++ gila-challenge/src/main/java/dlopezgarsco/db/CategoryDAO.java
@@ -1,14 +1,12 @@
 package dlopezgarsco.db;
 
-import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 import java.util.List;
 
 public interface CategoryDAO {
-
-    @SqlQuery("SELECT * FROM \"categories\"")
-    @RegisterConstructorMapper(Category.class)
+    @SqlQuery("SELECT * FROM categories")
+    @RegisterBeanMapper(Category.class)
     List<Category> selectAll();
-
 }
